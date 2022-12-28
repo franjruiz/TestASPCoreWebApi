6601a593b7c2866cec5b93850e776def47e8a0e0
Francisco Javier Ruiz <franciscor@cyc.es>
2022-12-28 10:05:59 +0100

    + Initial Db Context

---
+++ src/CompanyEmployees/ContextFactory/RepositoryContextFactory.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Repository;
+
+namespace CompanyEmployees.ContextFactory;
+
+/// <summary>
+/// Repository context factory
+/// </summary>
+public class RepositoryContextFactory : IDesignTimeDbContextFactory<RepositoryContext>
+{
+    /// <inheritdoc/>
+    public RepositoryContext CreateDbContext(string[] args)
+    {
+        var configuration = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json")
+            .Build();
+
+        var builder = new DbContextOptionsBuilder<RepositoryContext>()
+            .UseSqlServer(configuration.GetConnectionString("sqlConnection"));
+
+        return new RepositoryContext(builder.Options);
+    }
+}
---
+++ src/Repository/RepositoryContext.cs
@@ -0,0 +1,29 @@
+﻿using Entities.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Repository;
+
+/// <summary>
+/// Repository context.
+/// </summary>
+public class RepositoryContext : DbContext
+{
+    /// <summary>
+    /// Constructor.
+    /// </summary>
+    /// <param name="options"></param>
+    public RepositoryContext(DbContextOptions options)
+        : base(options)
+    {
+    }
+
+    /// <summary>
+    /// Companies.
+    /// </summary>
+    public DbSet<Company>? Companies { get; set; }
+
+    /// <summary>
+    /// Employees
+    /// </summary>
+    public DbSet<Employee>? Employees { get; set; }
+}
