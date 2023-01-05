a84e4d632ab54d60342ca1db5897648368a91253
Francisco Javier Ruiz <franciscor@cyc.es>
2023-01-05 10:37:58 +0100

    + Repository base

---
+++ src/Contracts/IRepositoryBase.cs
@@ -0,0 +1,43 @@
+﻿using System.Linq.Expressions;
+
+namespace Contracts;
+
+/// <summary>
+/// Repository base interface
+/// </summary>
+/// <typeparam name="T"></typeparam>
+public interface IRepositoryBase<T>
+{
+    /// <summary>
+    /// Find all
+    /// </summary>
+    /// <param name="trackChanges"></param>
+    /// <returns></returns>
+    IQueryable<T> FindAll(bool trackChanges);
+
+    /// <summary>
+    /// Find by condition
+    /// </summary>
+    /// <param name="expression"></param>
+    /// <param name="trackChanges"></param>
+    /// <returns></returns>
+    IQueryable<T> FindByCondition(Expression<Func<T, bool>> expression, bool trackChanges);
+
+    /// <summary>
+    /// Create
+    /// </summary>
+    /// <param name="entity"></param>
+    void Create(T entity);
+
+    /// <summary>
+    /// Update
+    /// </summary>
+    /// <param name="entity"></param>
+    void Update(T entity);
+
+    /// <summary>
+    /// Delete
+    /// </summary>
+    /// <param name="entity"></param>
+    void Delete(T entity);
+}
---
+++ src/Repository/RepositoryBase.cs
@@ -0,0 +1,57 @@
+﻿using System.Linq.Expressions;
+using Contracts;
+using Microsoft.EntityFrameworkCore;
+
+namespace Repository;
+
+/// <summary>
+/// Repository base
+/// </summary>
+/// <typeparam name="T"></typeparam>
+public abstract class RepositoryBase<T> : IRepositoryBase<T> where T : class
+{
+    /// <summary>
+    /// Repository context.
+    /// </summary>
+    protected RepositoryContext RepositoryContext;
+
+    /// <summary>
+    /// Constructor
+    /// </summary>
+    /// <param name="repositoryContext"></param>
+    protected RepositoryBase(RepositoryContext repositoryContext)
+    {
+        this.RepositoryContext = repositoryContext;
+    }
+
+    /// <inheritdoc/>
+    public void Create(T entity) => this.RepositoryContext.Set<T>().Add(entity);
+
+    /// <inheritdoc/>
+    public void Delete(T entity) => this.RepositoryContext.Set<T>().Remove(entity);
+
+    /// <inheritdoc/>
+    public IQueryable<T> FindAll(bool trackChanges)
+    {
+        if (!trackChanges)
+        {
+            return this.RepositoryContext.Set<T>().AsNoTracking();
+        }
+
+        return this.RepositoryContext.Set<T>();
+    }
+
+    /// <inheritdoc/>
+    public IQueryable<T> FindByCondition(Expression<Func<T, bool>> expression, bool trackChanges)
+    {
+        if (!trackChanges)
+        {
+            return this.RepositoryContext.Set<T>().Where(expression).AsNoTracking();
+        }
+
+        return this.RepositoryContext.Set<T>().Where(expression);
+    }
+
+    /// <inheritdoc/>
+    public void Update(T entity) => this.RepositoryContext.Set<T>().Update(entity);
+}
